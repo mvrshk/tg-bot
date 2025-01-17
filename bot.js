57045f348af1e992ba9164445bc86bf59d8b4bdf
Marina <marina@mail.ru>
2025-01-17 14:24:36 +0300

    игрушки

--- bot.js
+++ bot.js
@@ -4,35 +4,52 @@ const bot = new Bot('7707427846:AAGlk1n3_Yd-uYeVVsGR0a-6SF7HUrTIJ_g');
 
 bot.command('start', (ctx) => {
     const keyboard = new InlineKeyboard()
-        .text('1', 'var1')
-        .text('2', 'var2')
+        .text('Орел Решка', 'var1')
+        .text('Камень-бумага', 'var2')
+        .text('Угадай число', 'var3')
         .row()
         .text('помогите', 'help');
-        ctx.reply('Привет!  напиши /help, чтобы ботика на веранде оес!! есои прост на кнопки надо пожмякать, вперед',{reply_markup: keyboard});
+    ctx.reply('Привет! Напиши /help, чтобы узнать, как использовать бота. Нажми на кнопки, чтобы играть!', { reply_markup: keyboard });
 });
 
-bot.on('callback_query:data', (ctx) => {
+bot.on('callback_query:data', async (ctx) => {
     const data = ctx.callbackQuery.data;
 
-    if (data === 'var1'){
-        ctx.answerCallbackQuery('Неправильный вариант...');
-        ctx.reply('лучше выберите другой.');
-    }else  if (data === 'var2'){
-        ctx.answerCallbackQuery('верно!!! с нас 5к в подарок');
-        ctx.reply('лучше выберите другой.');
-    }else if (data === 'help'){
-        ctx.answerCallbackQuery('вам особо не помочь');
-        ctx.reply('помощь только через /help.');
+    if (data === 'var1') {
+        ctx.answerCallbackQuery('Тут 50/50...');
+
+        const game1Keyboard = new InlineKeyboard()
+            .text('Орел', 'earl')
+            .text('Решка', 'reshka');
+
+        ctx.reply('Выберите Орел или Решка', { reply_markup: game1Keyboard });
+    } else if (data === 'earl' || data === 'reshka') {
+        await playCoinTossGame(ctx, data);  
+    } else if (data === 'var2') {
+        ctx.answerCallbackQuery('Тут 33%...');
+
+        const game2Keyboard = new InlineKeyboard()
+            .text('Камен', 'petr')
+            .text('Ножны', 'scissors')
+            .text('Бугага', 'paper');
+
+            ctx.reply('Выберите Камен, Ножны или Бугага', { reply_markup: game2Keyboard });
+    }  else if (data === 'petr' || data === 'scissors' || data === 'paper') {
+        await playKMNGame(ctx, data);  
+    }else if (data === 'help') {
+        ctx.answerCallbackQuery('уже нечем помочь');
+        ctx.reply('нажми /help.');
     }
-})
+});
+
 
 bot.command('help', (ctx) => {
-    ctx.reply('/start - привет\n/help - помосчь\n/echo - повторить сообщение\n/jonk - спой песню');
+    ctx.reply('/start - привет\n/help - помощь\n/echo - повторить сообщение\n/jonk - спой песню');
 });
 
 bot.command('echo', (ctx) => {
     const message = ctx.message.text.split(' ').slice(1).join(' ');
-    ctx.reply(message||'напиши прикол, чтоб я copypaste тип смешной');
+    ctx.reply(message || 'Напиши прикол, чтобы я повторил.');
 });
 
 bot.command('jonk', async (ctx) => {
@@ -47,5 +64,37 @@ bot.command('jonk', async (ctx) => {
     await ctx.reply(randJonk);
 });
 
+async function playCoinTossGame(ctx, userChoice) {
+    const result = Math.random() < 0.5 ? 'Орел' : 'Решка';
+    const choice = userChoice === 'earl' ? 'Орел' : 'Решка';
+
+    if (choice === result) {
+        await ctx.reply(`Выбор: ${choice}. ww`);
+    } else {
+        await ctx.reply(`Выбор: ${choice}. =( Результат: ${result}`);
+    }
+}
+
+async function playKMNGame(ctx, userChoice) {
+    const options = ['Камен', 'Ножны', 'Бугага'];
+    const result = options[Math.floor(Math.random() * 3)];
+    const choice = userChoice === 'petr' ? 'Камен' : userChoice === 'scissors' ? 'Ножны' : 'Бугага';
+
+    let outcomeMessage = `Вы: ${choice}. ботяра: ${result}.`;
+
+    if (choice === result) {
+        outcomeMessage += ' Ничья!';
+    } else if (
+        (choice === 'Камен' && result === 'Ножны') ||
+        (choice === 'Ножны' && result === 'Бугага') ||
+        (choice === 'Бугага' && result === 'Камен')
+    ) {
+        outcomeMessage += ' ww';
+    } else {
+        outcomeMessage += ' =(';
+    }
+
+    await ctx.reply(outcomeMessage);
+}
 bot.start();
-console.log('bot starts.');
+console.log('Bot started.');
